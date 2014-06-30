79583244c00ad6fa15e339c7298e417af26428e0
Chai Feng <chaifeng@chaifeng.com>
2014-06-30 10:44:53 +0800

    把所有测试相关的代码移动到 FizzBuzzTest

--- src/main/java/com/baldurtech/miniature/spice/FizzBuzz.java
+++ src/main/java/com/baldurtech/miniature/spice/FizzBuzz.java
@@ -7,33 +7,4 @@ public class FizzBuzz {
         }
         return String.valueOf(num);
     }
-
-    static Boolean testResult = true;
-
-    public static void main(String[] args) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        assertEquals("1", fizzBuzz.say(1));
-
-        assertEquals("2", fizzBuzz.say(2));
-
-        assertEquals("Fizz", fizzBuzz.say(3));
-
-        outputTestReport();
-    }
-
-    public static void assertEquals(String expectedResult, String actualResult) {
-        if(false == expectedResult.equals(actualResult)) {
-            testResult = false;
-            System.out.println("Expected `" + expectedResult + "`, but `" + actualResult + "`");
-        }
-    }
-
-    private static void outputTestReport() {
-        if(testResult) {
-            System.out.println("TEST SUCCESS!");
-        } else {
-            System.out.println("FAIL!");
-        }
-    }
 }
---
+++ src/main/java/com/baldurtech/miniature/spice/FizzBuzzTest.java
@@ -0,0 +1,33 @@
+package com.baldurtech.miniature.spice;
+
+public class FizzBuzzTest {
+
+    static Boolean testResult = true;
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        assertEquals("1", fizzBuzz.say(1));
+
+        assertEquals("2", fizzBuzz.say(2));
+
+        assertEquals("Fizz", fizzBuzz.say(3));
+
+        outputTestReport();
+    }
+
+    public static void assertEquals(String expectedResult, String actualResult) {
+        if(false == expectedResult.equals(actualResult)) {
+            testResult = false;
+            System.out.println("Expected `" + expectedResult + "`, but `" + actualResult + "`");
+        }
+    }
+
+    private static void outputTestReport() {
+        if(testResult) {
+            System.out.println("TEST SUCCESS!");
+        } else {
+            System.out.println("FAIL!");
+        }
+    }
+}
