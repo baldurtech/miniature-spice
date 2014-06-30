856d74c0f18cf720a3e092910a9f15828490418b
Chai Feng <chaifeng@chaifeng.com>
2014-06-30 18:11:17 +0800

    增加一个 HelloWorldTest 测试案例

---
+++ src/test/java/com/baldurtech/miniature/spice/HelloWorldTest.java
@@ -0,0 +1,7 @@
+package com.baldurtech.miniature.spice;
+
+public class HelloWorldTest extends MiniatureSpiceTestCase {
+    public void test_1_should_equals_1() {
+        assertEquals("1", String.valueOf(1));
+    }
+}
