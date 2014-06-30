bbf48f8a8aa284a7b80da02ccfe2862a876a08a9
Chai Feng <chaifeng@chaifeng.com>
2014-06-30 10:09:00 +0800

    让测试 2 通过

--- src/main/java/com/baldurtech/miniature/spice/FizzBuzz.java
+++ src/main/java/com/baldurtech/miniature/spice/FizzBuzz.java
@@ -2,11 +2,12 @@ package com.baldurtech.miniature.spice;
 
 public class FizzBuzz {
     public String say(Integer num) {
-        return "1";
+        return String.valueOf(num);
     }
 
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         System.out.println("1".equals(fizzBuzz.say(1)));
+        System.out.println("2".equals(fizzBuzz.say(2)));
     }
 }
