578cca028041d0b369b5e868dbcd8733ee5651db
Chai Feng <chaifeng@chaifeng.com>
2014-06-30 16:51:10 +0800

    修复 num 为 4 的时候抛出异常的 bug

--- src/main/java/com/baldurtech/miniature/spice/FizzBuzz.java
+++ src/main/java/com/baldurtech/miniature/spice/FizzBuzz.java
@@ -6,8 +6,6 @@ public class FizzBuzz {
     public String say(Integer num) {
         if(num == 2) thisIsABug = true;
 
-        if(num == 4) throw new RuntimeException("We don't need 4!");
-
         if(num % 3 == 0) {
             if(thisIsABug) return "42";
             return "Fizz";
